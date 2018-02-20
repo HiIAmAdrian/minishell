6871c9fb79bb94d8a2645ca6dd5cc1a233dd9e51
Adrian STAN <adstan@b1p22.academyplus.ro>
2018-02-20 21:13:08 +0200

    deleted comms

--- exec_commands.c
+++ exec_commands.c
@@ -6,7 +6,7 @@
 /*   By: adstan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/17 12:24:18 by adstan            #+#    #+#             */
-/*   Updated: 2018/02/20 20:25:22 by adstan           ###   ########.fr       */
+/*   Updated: 2018/02/20 21:12:30 by adstan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int		run_commands(char ***arg)
 	{
 		if ((ft_strnequ(arg[i][0], "cd", 2)))
 			cd_builtin(arg[i]);
-//		i+=0;
 		else if ((ft_strnequ(arg[i][0], "echo", 4)))
 			echo_builtin(arg[i]);
 		else if ((ft_strnequ(arg[i][0], "env", 3)))
@@ -34,11 +33,6 @@ int		run_commands(char ***arg)
 			exit_sh();
 		else 
 			bin_exec(arg[i]);
-	/*	else
-		 {
-			 ft_putstr("minishell: command not found: ");
-			 ft_putendl(arg[i][0]);
-		 }*/
 		i++;
 	}
 	return(0);
